b153fb87be52a0148d88ca2592aec9b5dfafbb73
AwrelH <hiwa.awrel@gmail.com>
2022-09-29 12:25:11 +0000

    add task functionality to app, new template add_task

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,9 +2,26 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialiazation
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-  });
+  
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
+  
+    // modal
+    let elems = document.querySelectorAll('.modal');
     M.Modal.init(elems);
+  
+
+  
+    // datepicker
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format:"dd mmmm, yyyy",
+    }
+    );
+  
+
+
+  
+    // select category
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
   });
